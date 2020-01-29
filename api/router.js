d41ef832588f3c152544a4c521e652add55a7082
AiLlE72 <willy.paris72@gmail.com>
2020-01-29 12:22:32 +0100

    ajout page admin en cours et ajout marketplacecreate

--- api/router.js
+++ api/router.js
@@ -1,18 +1,21 @@
-/*
- * Router.js
- ***********/
+/************
+ * Router.js*
+ ************/
+
 // Import
 const express = require('express')
 const router = express.Router()
 
 
 // Import de controllers
-const home = require('./controllers/home')
-const signup = require('./controllers/signup')
-const marketplace = require('./controllers/marketplace')
-const wandw = require('./controllers/wandw')
-const actus = require('./controllers/actus')
-const actuSingle = require ('./controllers/actuSingle')
+const home                  = require('./controllers/home')
+const signup                = require('./controllers/signup')
+const marketplace           = require('./controllers/marketplace')
+const marketplaceCreate     = require('./controllers/marketplaceCreate')
+const wandw                 = require('./controllers/wandw')
+const actus                 = require('./controllers/actus')
+const actuSingle            = require ('./controllers/actuSingle')
+const admin                 = require ('./controllers/admin')
 
 // Home
 router.route('/')
@@ -26,6 +29,10 @@ router.route('/signup')
 router.route('/marketplace')
     .get(marketplace.get)
 
+// marketplace
+router.route('/marketplaceCreate')
+    .get(marketplaceCreate.get)
+
 // wandw
 router.route('/wandw')
     .get(wandw.get)
@@ -37,5 +44,11 @@ router.route('/actus')
 // actuSingle
 router.route('/actuSingle')
     .get(actuSingle.get)
+
+//adminpage
+router.route('/admin')
+    .get(admin.get)
+
+
 
 module.exports = router
