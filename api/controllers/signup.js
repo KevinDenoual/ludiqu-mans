2654f9909d9310ca2e5a8e5e4107c35f590fce1a
willy <willy.paris72@gmail.com>
2020-02-09 22:38:08 +0100

    essai d'affichage des erreur

--- api/controllers/signup.js
+++ api/controllers/signup.js
@@ -2,61 +2,44 @@ const usermodel = require('../database/models/userModel')
 
 module.exports = {
     get: (req, res) => {
-        res.render('signup', {
-            errors: req.flash('gestError')
-        })
+        res.render('signup', { errors: req.flash('gestError') })
     },
 
     postSignup: (req, res) => {
         const Pass = req.body.password
         const confPass = req.body.confPassword
         const check = req.body.gridCheck
-        
-
         usermodel.create(
             req.body, (error, user) => {
-                const gestError = Object.keys(error.errors).map(key => error.errors[key].message)
-                req.flash('gestError', gestError)
-                if (Pass !== confPass ||  check !== 'on' ) {
-
-                    res.redirect('/signup')
-                } else {
-                    usermodel.create(
-                        {
-                            email: req.body.email,
-                            name: req.body.name,
-                            password: Pass,
-                            isVerified: false,
-                            isModo: false,
-                            isAdmin: false,
-                            isBan: false,
-                        },
-                    )
-                    res.render('home')
+                if (error) {
+                    const gestError = Object.keys(error.errors).map(key => error.errors[key].message)
+                    req.flash('gestError', gestError)
+                    console.log('coucou');
+                    return res.redirect('/signup')
+                    console.log('recoucou');
+                    
+                    
+                    
                 }
-                
+                // next()
             })
+
+        if (Pass !== confPass || check !== 'on') {
+            res.redirect('/signup')
+        } else {
+            usermodel.create(
+                {
+                    email: req.body.email,
+                    name: req.body.name,
+                    password: Pass,
+                    isVerified: false,
+                    isModo: false,
+                    isAdmin: false,
+                    isBan: false,
+                },
+            )
+            res.render('home')
+        }
+
     }
-
-
-    // si dessus le sessai en cours selon la video 6.13 avec les fonction de recuperation d'erreur time : 5,13
-
-
-
-
-
-
-
-
-
-
-
-
-    // le code si desssous est fonctionnel 
-
-
-
-
-
 }
-
